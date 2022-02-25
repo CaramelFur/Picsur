eca47c994762b1b30ca16940ee48a5951744b4b4
rubikscraft <contact@rubikscraft.nl>
2022-02-25 22:58:08 +0100

    update package

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,10 +1,15 @@
 const scopedcss = require('craco-plugin-scoped-css');
+const path = require('path');
 
 module.exports = {
   devServer: {
-    writeToDisk: true,
+    devMiddleware: {
+      writeToDisk: true,
+    },
+
     port: 3300,
     liveReload: true,
+    hot: true,
     open: false,
   },
   plugins: [
