39a59e7b9b4cb9518bff15c35c4b4ddb29f06339
rubikscraft <contact@rubikscraft.nl>
2022-02-28 20:04:21 +0100

    add login router

--- frontend/src/routes/router.tsx
+++ frontend/src/routes/router.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
+import LoginView from './login/login';
 import ProcessingView from './processing/processing';
 import UploadView from './upload/upload';
 import ViewView from './view/view';
@@ -7,6 +8,7 @@ export default function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<UploadView />} />
+      <Route path="login" element={<LoginView />} />
       <Route path="processing" element={<ProcessingView />} />
       <Route path="view/:hash" element={<ViewView />} />
     </Routes>
