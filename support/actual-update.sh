f794c724c08f70cc4c5083e8625fc6930313b773
rubikscraft <contact@rubikscraft.nl>
2022-12-28 14:31:09 +0100

    Update dependencies, for real this time Just why doesn't yarn do this automatically? this is stupid

---
+++ support/actual-update.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Really yarn, why the hell am I supposed to do this?
+# If I tell you to upgrade, upgrade everything
+
+# Go to this script parent
+cd "$(dirname "${BASH_SOURCE[0]}")/.."
+
+# Print a list of all packages
+# Get ndjson stream from yarn
+# Strip the quotes from the package names
+# Strip the version from the package names
+# Concat all package names into a single line with spaces
+# Run "yarn up -R {all-packages}" to update all packages
+yarn info --name-only -R -A --json | \
+  jq | \
+  sed 's/\"//g' | \
+  sed 's/@.*//g' | \
+  sort | \
+  uniq | \
+  xargs yarn up -R
+
